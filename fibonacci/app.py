9bef714732aa6c1c8866fb7d0884b452fb956804
Eldar Kalachev <rusgunter@gmail.com>
2019-07-07 02:46:15 +0300

    Find the n-th Fibonacci element (n^2)
    added one test with a big number and a little bit refactoring

--- fibonacci/app.py
+++ fibonacci/app.py
@@ -1,6 +1,7 @@
 def fib_bu(n):
     if n <= 1:
         return n
+    
     prev, curr = 0, 1
 
     for i in range(n - 2):
@@ -16,6 +17,7 @@ def test():
     assert fib_bu(6) == 5
     assert fib_bu(7) == 8
     assert fib_bu(13) == 144
+    assert fib_bu(100) == 218922995834555169026
 
 
 if __name__ == "__main__":
